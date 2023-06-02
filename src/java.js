81610c04a767c72f5141e9c75d5e7539a06a8e35
kikicod3r <kitajjenkins@gmail.com>
2023-06-01 20:01:41 -0500

    Added Javascript

--- src/java.js
+++ src/java.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
 
+  let milanElement = document.querySelector("#milan");
+  let milanDateElement = milanElement.querySelector(".date");
+  let milanTimeElement = milanElement.querySelector(".time");
+  let milanTime = moment().tz("Europe/Rome");
+  milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
+  milanTimeElement.innerHTML = milanTime.format("h:mm:ss[<small>]A[</small>]");
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss[<small>]A[</small>]");
+}
+
+setInterval(updateTime, 1000);
