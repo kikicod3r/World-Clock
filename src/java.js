0f14dc169d274db52d366a9931b6423d29b7d068
kikicod3r <kitajjenkins@gmail.com>
2023-06-03 13:52:55 -0500

    Added current location option

--- src/java.js
+++ src/java.js
@@ -1,30 +1,43 @@
 function updateTime() {
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
-  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-  newYorkTimeElement.innerHTML = newYorkTime.format(
-    "h:mm:ss[<small>]A[</small>]"
-  );
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
 
   let milanElement = document.querySelector("#milan");
-  let milanDateElement = milanElement.querySelector(".date");
-  let milanTimeElement = milanElement.querySelector(".time");
-  let milanTime = moment().tz("Europe/Rome");
-  milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
-  milanTimeElement.innerHTML = milanTime.format("h:mm:ss[<small>]A[</small>]");
+  if (milanElement) {
+    let milanDateElement = milanElement.querySelector(".date");
+    let milanTimeElement = milanElement.querySelector(".time");
+    let milanTime = moment().tz("Europe/Rome");
+    milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
+    milanTimeElement.innerHTML = milanTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
 
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
-  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss[<small>]A[</small>]");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCityDisplay(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities-display");
